cd4ae6c99e13502b87f453c1349fb2bc1fef0217
Laura Simmons <laura30simmons@gmail.com>
2015-08-24 21:08:18 -0400

    more devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :signed_in?
   protect_from_forgery with: :exception
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
   Dir['app/services/*.rb'].each do |f|
     basename = File.basename(f)
     require_relative "../services/#{basename}"
@@ -20,6 +22,16 @@ class ApplicationController < ActionController::Base
     unless signed_in?
       flash[:notice] = 'You must sign in'
       redirect_to root_path
+    end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
+
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:password, :password_confirmation, :current_password, :username)
     end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  protected
+
+  def user_params
+    params.require(:user)
+  end
 end
